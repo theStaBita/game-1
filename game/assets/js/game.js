e63808fb8fa718844f490aaffcb13a79fde1f7c4
Elijah C. Voigt <elijah.caine@coreos.com>
2017-02-17 12:43:50 -0800

    Adds Phaser and simple assets framework.

---
+++ game/assets/js/game.js
@@ -0,0 +1,17 @@
+let run_game = function() {
+
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+
+    function preload () {
+
+        game.load.image('logo', 'assets/img/phaser.png');
+
+    }
+
+    function create () {
+
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+
+    }
+}
