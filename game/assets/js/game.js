fce85562e9efa0b4b3f78cd803755f97a9ca4b7e
Elijah C. Voigt <elijah.caine@coreos.com>
2017-02-25 13:05:05 -0800

    Initial attempt to make something playable.

--- game/assets/js/game.js
+++ game/assets/js/game.js
@@ -1,17 +1,43 @@
 let run_game = function() {
 
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'id-game', { preload: preload, create: create , update: update });
 
     function preload () {
-
-        game.load.image('logo', 'assets/img/phaser.png');
-
+        game.load.image('player','assets/img/roguelike/Girl-Mage-Single.png');
+        game.load.image('item','assets/img/roguelike/Item-Single.png');
+        game.load.image('wall', 'assets/img/roguelike/Tileset-Single.png');
+        game.load.image('ledge', 'assets/img/roguelike/Ledge-Single.png');
     }
 
+    var platforms;
+
     function create () {
+        var sprites = {};
 
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
+        sprites.item = game.add.sprite(0, 0, 'item');
+        sprites.wall = game.add.sprite(game.width/2, 0, 'wall');
+        sprites.ledge = game.add.sprite(game.width/2, 0, 'ledge');
+        sprites.player = game.add.sprite(0, 100, 'player');
+
+        for (s in sprites) {
+            sprites[s].smoothed = false;
+        }
+        
+        platforms = game.add.group();
+        platforms.enableBody = true;
+        var ground = platforms.create(0, game.world.height - 64, 'wall');
+
+        ground.body.immovable = true;
+        ground.scale.setTo(6,6);
+
+        let ledges = []
+        ledge = platforms.create(400, 400, 'ledge');
+        ledge.body.immovable = true;
+        ledge.scale.setTo(6,6);
+    }
+
+    function update() {
     }
 }
