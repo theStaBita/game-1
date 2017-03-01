a89343f2315a3c759b9a439be7b6f50a0105ec59
Elijah C. Voigt <elijah.caine@coreos.com>
2017-03-01 12:55:45 -0800

    Holy shit you can play it!

--- game/assets/js/game.js
+++ game/assets/js/game.js
@@ -5,39 +5,84 @@ let run_game = function() {
     function preload () {
         game.load.image('player','assets/img/roguelike/Girl-Mage-Single.png');
         game.load.image('item','assets/img/roguelike/Item-Single.png');
-        game.load.image('wall', 'assets/img/roguelike/Tileset-Single.png');
+        game.load.image('brick', 'assets/img/roguelike/Tileset-Single.png');
         game.load.image('ledge', 'assets/img/roguelike/Ledge-Single.png');
     }
 
-    var platforms;
+    let platforms
 
     function create () {
-        var sprites = {};
-
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        sprites.item = game.add.sprite(0, 0, 'item');
-        sprites.wall = game.add.sprite(game.width/2, 0, 'wall');
-        sprites.ledge = game.add.sprite(game.width/2, 0, 'ledge');
-        sprites.player = game.add.sprite(0, 100, 'player');
-
-        for (s in sprites) {
-            sprites[s].smoothed = false;
-        }
-        
         platforms = game.add.group();
         platforms.enableBody = true;
-        var ground = platforms.create(0, game.world.height - 64, 'wall');
 
-        ground.body.immovable = true;
-        ground.scale.setTo(6,6);
+        var ground = []
+        for (i=0; i<15; i++) {
+            ground.push(platforms.create((i*56), game.world.height - 60, 'brick'));
+            ground[i].scale.setTo(7, 7);
+            ground[i].smoothed = false;
+            ground[i].body.immovable = true;
+        }
 
-        let ledges = []
-        ledge = platforms.create(400, 400, 'ledge');
+        var ledge = platforms.create(400, 400, 'ledge');
         ledge.body.immovable = true;
-        ledge.scale.setTo(6,6);
+        ledge.immovable = true;
+        ledge.smoothed = false;
+        ledge.scale.setTo(7,7);
+        ledge = platforms.create(150, 250, 'ledge');
+        ledge.smoothed = false;
+        ledge.body.immovable = true;
+        ledge.scale.setTo(7,7);
+
+        player = game.add.sprite(32, game.world.height - 150, 'player');
+        game.physics.arcade.enable(player);
+
+        player.smoothed = false;
+        player.scale.setTo(7,7);
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 300;
+        player.body.collideWorldBounds = true;
+
+        //  Our two animations, walking left and right.
+        // player.animations.add('left', [0, 1, 2, 3], 10, true);
+        // player.animations.add('right', [5, 6, 7, 8], 10, true);
     }
 
     function update() {
+        var hitPlatform = game.physics.arcade.collide(player, platforms);
+
+        let cursors = game.input.keyboard.createCursorKeys();
+
+        //  Reset the players velocity (movement)
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown)
+        {
+            //  Move to the left
+            player.body.velocity.x = -150;
+
+            // player.animations.play('left');
+        }
+        else if (cursors.right.isDown)
+        {
+            //  Move to the right
+            player.body.velocity.x = 150;
+
+            // player.animations.play('right');
+        }
+        else
+        {
+            //  Stand still
+            player.animations.stop();
+
+            player.frame = 4;
+        }
+
+        //  Allow the player to jump if they are touching the ground.
+        if (cursors.up.isDown && player.body.touching.down && hitPlatform)
+        {
+            player.body.velocity.y = -350;
+        }
     }
 }
